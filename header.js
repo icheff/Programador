aff27fd112f49ea01f8f9628e90be66c61df281e
icheff <icheff.com@gmail.com>
2024-01-17 18:41:15 +0100

    Update

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
 fetch('./header')
 .then(response => response.ok ? response.text() : Promise.reject('Not found'))
-.then(headerData => document.getElementById(header').innerHTML = headerData)
+.then(headerData => document.getElementById('header').innerHTML = headerData)
 .catch(error => console.error(error.message));
