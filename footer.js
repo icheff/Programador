a835191d8f15df27910eae688876cf651cc3843c
icheff <icheff.com@gmail.com>
2024-01-17 17:54:39 +0100

    Update

--- footer.js
+++ footer.js
@@ -1,5 +1,4 @@
-// footer.js
 fetch('./footer')
-  .then(response => response.ok ? response.text() : Promise.reject('Not found'))
-  .then(footerData => document.getElementById('footer').innerHTML = footerData)
-  .catch(error => console.error(error.message));
+.then(response => response.ok ? response.text() : Promise.reject('Not found'))
+.then(footerData => document.getElementById('footer').innerHTML = footerData)
+.catch(error => console.error(error.message));
