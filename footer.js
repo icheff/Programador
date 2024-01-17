af84e9a9bbc1b0d0814d5e82b367d394915ad4e2
icheff <icheff.com@gmail.com>
2024-01-17 17:14:31 +0100

    Update

---
+++ footer.js
@@ -0,0 +1,5 @@
+// footer.js
+fetch('footer/footer.html')
+  .then(response => response.ok ? response.text() : Promise.reject('El archivo HTML del footer no se encontró.'))
+  .then(footerData => document.getElementById('footerContainer').innerHTML = footerData)
+  .catch(error => console.error(error.message));
