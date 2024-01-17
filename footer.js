9738bcde03befef4db042ff1ee825bb1fe01c7ee
icheff <icheff.com@gmail.com>
2024-01-17 17:44:54 +0100

    Update

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 // footer.js
-fetch('footer/')
+fetch('./footer/index.html')
   .then(response => response.ok ? response.text() : Promise.reject('Not found'))
   .then(footerData => document.getElementById('footerContainer').innerHTML = footerData)
   .catch(error => console.error(error.message));
