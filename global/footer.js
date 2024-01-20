6ffb744793cc15fa245a6c25b17b3e1595f8bdba
icheff <icheff.com@gmail.com>
2024-01-20 13:17:50 +0100

    Update

--- global/footer.js
+++ global/footer.js
@@ -1,4 +1,4 @@
-fetch('../footer')
+fetch('./footer')
 .then(response => response.ok ? response.text() : Promise.reject('Not found'))
 .then(footerData => document.getElementById('footer').innerHTML = footerData)
 .catch(error => console.error(error.message));
