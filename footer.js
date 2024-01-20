02a98e17ad340ae3afbb51731269149d4eaf0f65
icheff <octocat@github.com>
2024-01-20 13:30:06 +0100

    Delete file

--- footer.js
+++
@@ -1,4 +0,0 @@
-fetch('./footer')
-.then(response => response.ok ? response.text() : Promise.reject('Not found'))
-.then(footerData => document.getElementById('footer').innerHTML = footerData)
-.catch(error => console.error(error.message));
